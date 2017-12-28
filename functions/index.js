7041b0f7ed4e944b53fcb3d95a45573c14225bb0
Alvaro Viebrantz <alvarowolfx@gmail.com>
2017-12-28 09:04:09 -0300

    [feat] Change device config API

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const Geopoint = require('geopoint');
+const googleapis = require('googleapis');
 
 admin.initializeApp(functions.config().firebase);
 
@@ -8,6 +9,87 @@ const GeoPointFirestore = admin.firestore.GeoPoint;
 const db = admin.firestore();
 
 const DISTANCE_THRESHOLD = 0.05; // 50 meters
+
+const API_SCOPES = ['https://www.googleapis.com/auth/cloud-platform'];
+const API_VERSION = 'v1beta1';
+const DISCOVERY_API = 'https://cloudiot.googleapis.com/$discovery/rest';
+const SERVICE_NAME = 'cloudiot';
+const DISCOVERY_URL = `${DISCOVERY_API}?version=${API_VERSION}`;
+
+const PROJECT_ID = process.env.GCLOUD_PROJECT;
+const REGION = 'us-central1';
+const REGISTRY = 'asset-tracker-registry';
+
+function getCloudIoTClient() {
+  return new Promise((resolve, reject) => {
+    googleapis.auth.getApplicationDefault((err, auth, projectId) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      googleapis.discoverAPI(DISCOVERY_URL, { auth }, (err, service) => {
+        if (!err) {
+          resolve(service);
+        } else {
+          reject(err);
+        }
+      });
+    });
+  });
+}
+
+exports.updateDeviceConfig = functions.https.onRequest((req, res) => {
+  let { deviceId, updateInterval } = req.query;
+
+  if (!deviceId) {
+    res.json({ err: 'Param `deviceId` is required!' });
+    return;
+  }
+
+  updateInterval = parseInt(updateInterval, 10);
+  if (!updateInterval) {
+    res.json({ err: 'Param `updateInterval` is required!' });
+    return;
+  }
+
+  getCloudIoTClient()
+    .then(client => {
+      const device_name = `projects/${PROJECT_ID}/locations/${REGION}/registries/${REGISTRY}/devices/${deviceId}`;
+      console.log(device_name);
+
+      const newConfig = { update_interval: updateInterval };
+      const data = new Buffer(JSON.stringify(newConfig), 'utf-8');
+      const binaryData = data.toString('base64');
+
+      client.projects.locations.registries.devices.modifyCloudToDeviceConfig(
+        {
+          name: device_name,
+          resource: {
+            version_to_update: 0,
+            data: {
+              binary_data: binaryData
+            }
+          }
+        },
+        (err, response) => {
+          if (!err) {
+            let deviceRef = db.collection('devices').doc(deviceId);
+            deviceRef.set(
+              {
+                updateInterval
+              },
+              { merge: true }
+            );
+          }
+          res.json({ err, response });
+        }
+      );
+    })
+    .catch(err => {
+      res.json({ err });
+    });
+});
 
 /**
  * Receive data from pubsub, then
@@ -53,12 +135,14 @@ exports.receiveTelemetry = functions.pubsub
       let distanceKm = lastLocation.distanceTo(newLocation, true);
 
       if (distanceKm >= DISTANCE_THRESHOLD) {
+        data.state = 'MOVING';
         return Promise.all([
-          updateCurrentDataFirestore(deviceRef, data, 'MOVING'),
+          updateCurrentDataFirestore(deviceRef, data),
           insertLocationLog(deviceRef, data)
         ]);
       } else {
-        return updateCurrentDataFirestore(deviceRef, data, 'STOPPED');
+        data.state = 'STOPPED';
+        return updateCurrentDataFirestore(deviceRef, data);
       }
     });
   });
@@ -66,13 +150,12 @@ exports.receiveTelemetry = functions.pubsub
 /**
  * Maintain last status in Firestore
  */
-function updateCurrentDataFirestore(deviceRef, data, state) {
+function updateCurrentDataFirestore(deviceRef, data) {
   let location = new GeoPointFirestore(data.latitude, data.longitude);
 
   return deviceRef.set(
     {
       location,
-      state,
       lastTimestamp: data.timestamp,
       speed: data.speed,
       deviceTemperature: data.deviceTemperature,
